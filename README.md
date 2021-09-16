050bd41d9c55a7eea368245c4bb0c6fe24670776
katerz avocado <ktrinaapodaca@yahoo.com>
2021-09-16 00:19:19 -0600

    updating readme

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
-# workday_scheduler
+# Day Planner
+
+![license badge](https://img.shields.io/badge/License-MIT-lightgrey.svg)
+
+## Table of Contents:
+
+- [License](#license)
+- [Description](#description)
+- [Installation](#installation)
+- [Test](#test)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Questions](#questions)
+
+## License
+
+This project is licensed under the MIT license.
+
+## Description
 
 This is a Weekday scheduler. You will be able to add things you are needing to do for the day. This calendar has the ablity to save your to-dos as well.
 
 For the entire day, it is divided into sections. Your calendar will display the current hour in red leaving the previous part of your day grey and the rest of your day in green.
 
 This calendar will also display the current day, month and year.
+
+## Tests
+
+undefined
+
+## Usage
+
+Adding a to-do list that will save and delete what has been completed as well as being aligned with the current time and date.
+
+## Contribution
+
+We believe code is never finished, and welcome your contributions to enhance the application's functionality. Please adhere to the Code of Conduct for the Contributor Covenant, version 2.0, at https://www.contributor-covenant.org/version/2/0/code_of_conduct.html.
+
+## Questions
+
+If you have any questions please contact me at ktrinaapodaca@yahoo.com You can find more of my projects at [kapodaca0403](https://github.com/kapodaca0403)
+
+## Installation
+
+Npi I
--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-var currDay = new Date();
-var newday =
-  currDay.getFullYear() +
-  "/" +
-  (currDay.getMonth() + 1) +
-  "/" +
-  currDay.getDate();
-document.getElementById("currentDay").innerHTML = currDay;
+$(document).ready(function () {
+  var currDay = new Date();
+  var newday =
+    currDay.getFullYear() +
+    "/" +
+    (currDay.getMonth() + 1) +
+    "/" +
+    currDay.getDate();
+  document.getElementById("currentDay").innerHTML = currDay;
+  console.log(currDay);
+  //var bttn = document.createElement("button");
+  //bttn.innerHTML = "Click Me";
+  //document.body.appendChild(bttn);
+
+  // target each timeblock by div id
+  // compare timeblocks to the current hour
+  // split current day into date and time , split time into hours mins, secs
+  // compare current days hours to the current hour on the timeblock , set a class
+  // each time block will need to have an input field , target the input field by ID
+  // click listener on each input field
+  // save button needs click listener, will set the input field text into local storage, key will be input field ID and value will be user input
+  // on page load, any info from local storage will populate time blocks "get"
+  function timecompare() {
+    var currentHour = moment().hours();
+
+    $(".block").each(function () {
+      var hourblock = parseInt($(this).attr("id").split("-")[1]);
+
+      if (hourblock < currentHour) {
+        $(this).addClass("past");
+      } else if (hourblock === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  timecompare();
+
+  $;
+});
